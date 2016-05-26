6fe5f141eb9a7ffff860e191209a5d636a135afd
Kemal Aydin <kaydin123@posta.pau.edu.tr>
2016-05-27 01:37:12 +0300

    Sonunda

---
+++ SosyalKaldık/SosyalKaldık/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SosyalKaldık.Startup))]
+namespace SosyalKaldık
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
