90d8ae74e780da0529e87ade1c9528f408608d82
Kemal Aydin <kaydin123@posta.pau.edu.tr>
2016-05-28 18:37:03 +0300

    commit

--- SosyalKaldık/SosyalKaldık/Models/IdentityModels.cs
+++ SosyalKaldık/SosyalKaldık/Models/IdentityModels.cs
@@ -14,6 +14,7 @@ namespace SosyalKaldık.Models
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
+            int a;
             return userIdentity;
         }
     }
